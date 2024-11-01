57940005a91f940de5b3abbdeeb7cb300ba8c42f
Sarneet K <126042654+tidalwave6@users.noreply.github.com>
2024-11-01 23:53:26 +1100

    Update StaffUserServlet.java

--- online-bookstore-app/src/main/java/com/g3app/controller/StaffUserServlet.java
+++ online-bookstore-app/src/main/java/com/g3app/controller/StaffUserServlet.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
